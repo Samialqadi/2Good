953185157ee06ccc087c23d5c279747675588495
Christopher Ariagno <christopherariagno@gmail.com>
2020-01-09 11:32:55 -0500

    Get charities finished

--- server/server.js
+++ server/server.js
@@ -2,7 +2,38 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = process.env.PORT
+const request = require('request');
+var admin = require("firebase-admin");
+var serviceAccount = require('./service-account.json');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://capital-one-19531.firebaseio.com"
+});
+
+var db = admin.database();
+
+
+// Charity Endpoints
+app.get('/v0/charity/getCharities', function (req, res) {
+    var options = { method: 'GET',
+    url: 'https://api.data.charitynavigator.org/v2/Organizations',
+    qs: 
+     { app_id: process.env.CHARITY_APP_ID,
+       app_key: process.env.CHARITY_APP_KEY,
+       pageSize: 5,
+       rated: true,
+       sort:"RATING:DESC"
+     }
+    };
+    request(options, function (error, response, body) {
+    if (error) throw new Error(error);  
+    res.send(JSON.parse(body))
+  });
+})
 
 app.listen(port, () => console.log(`Trash State School Coder server running on: ${port}!`))
+
+// db.ref('/').once('value').then(function(snapshot) {
+//     console.log(snapshot.val())
+// })
