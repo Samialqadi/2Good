cd1337ac4b35eda0736b49be78fa275f4d9289d6
wpdou <wpdoughe@buffalo.edu>
2020-01-09 15:07:40 -0500

    Develop

--- app/app/src/main/java/com/dawidjk2/sesfrontend/Quiz1Activity.java
+++ app/app/src/main/java/com/dawidjk2/sesfrontend/Quiz1Activity.java
@@ -26,53 +26,11 @@ import android.widget.LinearLayout;
 import java.util.ArrayList;
 
 public class Quiz1Activity extends AppCompatActivity {
-    public ArrayList<Charity> charityList = new ArrayList<>();
-    TextView textView;
-
-    public String url = "https://api.data.charitynavigator.org/v2/Organizations?app_id=a8597fc8&app_key=e2f022d55899528abddc3181808a6c94&rated=true";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz1);
-
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest
-                (Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
-
-                    @Override
-                    public void onResponse(JSONArray array) {
-                        ArrayList<String> stringArrayList = new ArrayList<>();
-                        try {
-                            for(int i = 0; i < array.length(); ++i) {
-                                JSONObject object = array.getJSONObject(i);
-                                Charity charity = new Charity();
-                                charity.charityName = object.getString("charityName");//name
-                                charity.websiteURL = object.getString("websiteURL");//website
-                                JSONObject cause = object.getJSONObject("cause");//cause
-                                charity.generalCause = cause.getString("causeName");
-                                charity.mission = object.getString("mission");//mission
-                                charity.tagline = object.getString("tagLine"); //tag line
-                                JSONObject rating = object.getJSONObject("currentRating");//rating
-                                charity.rating = rating.getInt("rating");//rating
-                                charityList.add(charity);
-                                stringArrayList.add(rating.getInt("rating") + "\n");
-                            }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-
-                    }
-                }, new Response.ErrorListener() {
-
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        /* TODO: Handle error */
-                        Log.e("Volley", error.toString());
-                    }
-                });
-
-        // Access the RequestQueue through your singleton class.
-        ApiSingleton.getInstance(this).addToRequestQueue(jsonArrayRequest);
 
         addListenerOnButton();
     }
--- app/app/src/main/java/com/dawidjk2/sesfrontend/Quiz2Activity.java
+++ app/app/src/main/java/com/dawidjk2/sesfrontend/Quiz2Activity.java
@@ -4,17 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.LinearLayout;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.dawidjk2.sesfrontend.Classes.Charity;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Quiz2Activity extends AppCompatActivity {
     List<String> habits;
+    public ArrayList<Charity> charityList = new ArrayList<>();
+    public String url = "https://api.data.charitynavigator.org/v2/Organizations?app_id=a8597fc8&app_key=e2f022d55899528abddc3181808a6c94&rated=true";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,5 +62,45 @@ public class Quiz2Activity extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void getCharities() {
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+
+                    @Override
+                    public void onResponse(JSONArray array) {
+                        ArrayList<String> stringArrayList = new ArrayList<>();
+                        try {
+                            for(int i = 0; i < array.length(); ++i) {
+                                JSONObject object = array.getJSONObject(i);
+                                Charity charity = new Charity();
+                                charity.charityName = object.getString("charityName");//name
+                                charity.websiteURL = object.getString("websiteURL");//website
+                                JSONObject cause = object.getJSONObject("cause");//cause
+                                charity.generalCause = cause.getString("causeName");
+                                charity.mission = object.getString("mission");//mission
+                                charity.tagline = object.getString("tagLine"); //tag line
+                                JSONObject rating = object.getJSONObject("currentRating");//rating
+                                charity.rating = rating.getInt("rating");//rating
+                                charityList.add(charity);
+                                stringArrayList.add(rating.getInt("rating") + "\n");
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        /* TODO: Handle error */
+                        Log.e("Volley", error.toString());
+                    }
+                });
+
+        // Access the RequestQueue through your singleton class.
+        ApiSingleton.getInstance(this).addToRequestQueue(jsonArrayRequest);
     }
 }
